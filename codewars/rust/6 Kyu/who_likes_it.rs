ab72288be7cacaecfeffcf86d7a05f2ea29be96e
Vadim Kirpa <ivadik2323@gmail.com>
2023-03-05 00:43:04 +0300

    Add who_likes_it

---
+++ codewars/rust/6 Kyu/who_likes_it.rs
@@ -0,0 +1,11 @@
+// Kata's link: https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+fn likes(names: &[&str]) -> String {
+    match names.len() {
+        0 => "no one likes this".to_string(),
+        1 => format!("{} likes this", names[0]),
+        2 => format!("{} and {} like this", names[0], names[1]),
+        3 => format!("{}, {} and {} like this", names[0], names[1], names[2]),
+        _ => format!("{}, {} and {} others like this", names[0], names[1], names.len() - 2),
+    }
+}
